17a4393a7ebffd4c65d6aa576749405cdd557631
Jesse Mansbridge <jessemansbridge@hotmail.com>
2022-03-09 09:52:33 +1030

    More functionality

--- Referee.cpp
+++ Referee.cpp
@@ -7,7 +7,13 @@ Referee::Referee() {
 };
 
 char Referee::refGame(Human player1, Computer player2) {
-  std::cout << player1.makeMove() << std::endl;
-  std::cout << player2.makeMove() << std::endl;
-  return 'w';
+  char player1Move = player1.makeMove();
+  char player2Move = player2.makeMove();
+
+  if (player1Move == player2Move) {
+    return 'T';
+  } else if ((player1Move == 'R' && player2Move == 'P') || (player1Move == 'P' && player2Move == 'S') || (player1Move == 'S' && player2Move == 'R')) {
+    return 'L';
+  }
+  return 'W';
 };
