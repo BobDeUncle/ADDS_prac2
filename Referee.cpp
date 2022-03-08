f3b176571b62e1ca12e518915e433ad61481434c
Jesse Mansbridge <jessemansbridge@hotmail.com>
2022-03-09 09:55:59 +1030

    Fixed Edge case

--- Referee.cpp
+++ Referee.cpp
@@ -10,7 +10,10 @@ char Referee::refGame(Human player1, Computer player2) {
   char player1Move = player1.makeMove();
   char player2Move = player2.makeMove();
 
-  if (player1Move == player2Move) {
+  if (player1Move != 'R' || player1Move != 'R' || player1Move != 'R') {
+    // Returns error if invalid input from player
+    return 'E';
+  } else if (player1Move == player2Move) {
     return 'T';
   } else if ((player1Move == 'R' && player2Move == 'P') || (player1Move == 'P' && player2Move == 'S') || (player1Move == 'S' && player2Move == 'R')) {
     return 'L';
