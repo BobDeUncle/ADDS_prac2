5e72608050a4df8b1ccfdbdf83f3a561aeb860a6
Jesse Mansbridge <jessemansbridge@hotmail.com>
2022-03-09 09:37:11 +1030

    First edits

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "Human.h"
+#include "Computer.h"
+
+using namespace std;
+
+int main(void) {
+  cout << "Test" << endl;
+  Human John;
+  cout << John.makeMove() << endl;
+}
+
